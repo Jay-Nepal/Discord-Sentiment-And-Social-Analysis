b0320aea4b7bb0625396dfb9f114335e058a6d40
JayNepal <lettertoaarogya@gmail.com>
2024-05-06 11:33:20 +0545

    Implement social graph output

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
 from interactions import (Client, Intents, listen,
                           slash_command, slash_option, OptionType,
-                          AutoDefer)
+                          AutoDefer, File)
 from interactions.api.events import Startup
+import pandas as pd
+import matplotlib.pyplot as plt
+import io
 import os
+import networkx as nx
 from dotenv import load_dotenv
 load_dotenv()
 
+# Initial definitions
 client = Client(intents=Intents.ALL)
 auto_defer_without_ephemeral = AutoDefer(enabled=True, ephemeral=False, time_until_defer=2.0)
 
 
+def make_social_network_graph(graph, all_relation):
+    # Adding edges to create a directional social network graph
+    graph.clear()
+    for index, row in all_relation.iterrows():
+        source = row['sender']
+        target = row['receiver']
+        strength = row['strength']
+        graph.add_edge(source, target, strength=strength)
+
+    # Generating a layout for the graph
+    pos = nx.spring_layout(graph)
+
+    # Drawing, Saving and Returning the graph
+    nx.draw(graph, pos, with_labels=True, node_color='skyblue', edge_color='k', font_size=12)
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format="PNG")
+    buffer.seek(0)
+    plt.close()
+    return buffer
+
+
+# Default startup option. Currently set to check servers the bot is present in
 @listen(Startup)
 async def startup_func():
     print("Joined Guilds:")
@@ -17,7 +44,7 @@ async def startup_func():
         print(guild.name)
 
 
-@slash_command(name="social_graph")
+@slash_command(name="social_network_graph")
 @slash_option(
     name="channel_id",
     description="ID of the channel to analyse",
@@ -26,17 +53,45 @@ async def startup_func():
 )
 async def fetch_history(ctx, channel_id: str):
     await AutoDefer.defer(auto_defer_without_ephemeral, ctx)
+    all_user_pair = []
+
     try:
         channel = await client.fetch_channel(int(channel_id))
         if channel is not None:
-            messages = await channel.history().flatten()
+            messages = await channel.history(limit=0).flatten()  # Gets all messages from a channel
+            print(len(messages))
             for message in messages:
-                await ctx.send(content=f"{message.author}: {message.content}")
-        else:
+                mentioned_users = set()
+                author = message.author
+                message_reply_to = message.get_referenced_message()
+                async for user in message.mention_users:
+                    mentioned_users.add(user.display_name)
+                if message_reply_to:
+                    mentioned_users.add(message_reply_to.author.display_name)
+                if len(mentioned_users) > 0:  # Only creating a social pair if there is a sender and receiver
+                    sender = author.display_name
+                    for receiver in mentioned_users:
+                        if receiver != sender:
+                            user_pair = {'sender': sender, 'receiver': receiver}
+                            all_user_pair.append(user_pair)
+        else:  # If the channel ID is integer but not found
             await ctx.send(content="Channel not found, please re-check permissions and ID.")
-    except ValueError:
+    except ValueError:  # If someone gives non-integer input for channel ID
         await ctx.send(content="Invalid channel ID, did you add a letter accidentally?")
 
+    # Converting user pair to a dataframe with strength for the number of times it repeated
+    df_network = pd.DataFrame(all_user_pair)
+    grouped = df_network.groupby(df_network.columns.difference(['strength']).tolist())
+    df_network['strength'] = grouped.transform('size')
+    df_network = df_network.drop_duplicates()
+    # Create a new graph object
+    G = nx.DiGraph()
+    # Get the social graph image as a buffer
+    buffer = make_social_network_graph(G, df_network)
+    print(df_network)
+    await ctx.send(file=File(buffer, file_name="graph.png"))
+    buffer.close()
+    await ctx.send('Finished', ephemeral=True)
 
 # Command to bulk delete messages for testing
 @slash_command(name="delete_all")
